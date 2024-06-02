e5e40aa17dac86d5d209c9615669d426992347e1
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-06-03 02:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
   //your code here
+
+	let ans=0,mn=10000000000;
+
+	for(let i=0;i<arr.length-2;i++){
+        for(let j=i+1;j<arr.length-1;j++){
+            for(let k=j+1;k<arr.length;k++){
+                let sm = arr[i]+arr[j]+arr[k];
+                if(Math.abs(sm-target)<mn){
+                    mn=Math.abs(sm-target);
+                    ans=sm;
+                }
+            }
+        }
+    }
+    return ans;
+	
 }
 
 module.exports = threeSum;
